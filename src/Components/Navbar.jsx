8cb864e265eccc5ea74ed84c0d524c71d6553493
YaredAlex <yareda25@gmail.com>
2023-12-21 01:20:37 +0530

    making nav-bar responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,80 +2,72 @@ import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "../Css/Navbar.css";
 import "../App.css";
+import { MdMenu } from "react-icons/md";
+import { useState } from "react";
 const Navbar = () => {
-	return (
-		<Container className="nav-container ">
-			<div
-				className="logo"
-				style={{ position: "relative" }}
-			>
-				<p className="fw-bolder logo ">
-					<span
-						style={{
-							position: "absolute",
-							top: "50%",
-							transform: "translateY(-50%)",
-						}}
-					>
-						LOGO
-					</span>
-				</p>
-			</div>
-			<div className="center">
-				<ul className="list-unstyled d-flex align-items-center m-0 ">
-					<li className="c text-decoration-none">
-						<Link
-							to="/"
-							className="text-decoration-none"
-							style={{ color: "#5ecc5c" }}
-						>
-							Home
-						</Link>
-					</li>
-					<li className="c">
-						<Link
-							to="/services"
-							className="text-decoration-none"
-							style={{ color: "#5ecc5c" }}
-						>
-							Services
-						</Link>
-					</li>
-					<li className="c">
-						<Link
-							to="/products"
-							className="text-decoration-none"
-							style={{ color: "#5ecc5c" }}
-						>
-							Products
-						</Link>
-					</li>
-					<li className="c">
-						<Link
-							to="/team"
-							className="text-decoration-none"
-							style={{ color: "#5ecc5c" }}
-						>
-							Team
-						</Link>
-					</li>
-					<li className="c">
-						<Link
-							to="/blogs"
-							className="text-decoration-none"
-							style={{ color: "#5ecc5c" }}
-						>
-							Blogs
-						</Link>
-					</li>
-				</ul>
-			</div>
-			<div className="cta d-flex gap ">
-				<button className="buttonlightgreen }">Login</button>
-				<button className="buttongreen">Sign Up</button>
-			</div>
-		</Container>
-	);
+  const [popMenu, setPopMenu] = useState(false);
+
+  const showMenu = () => {
+    setPopMenu(!popMenu);
+    console.log("clicked");
+  };
+  return (
+    <Container className="nav-container ">
+      <div className="logo" style={{ position: "relative" }}>
+        <p className="fw-bolder logo ">
+          <span
+            style={{
+              position: "absolute",
+              top: "50%",
+              transform: "translateY(-50%)",
+            }}
+          >
+            LOGO
+          </span>
+        </p>
+      </div>
+      <div className="center nav-list">
+        <ul
+          className={`list-unstyled d-flex align-items-center m-0 ${
+            popMenu ? "active " : ""
+          }`}
+        >
+          <li className=" text-decoration-none">
+            <Link to="/" className="text-decoration-none" style={{}}>
+              Home
+            </Link>
+          </li>
+          <li className="c">
+            <Link to="/services" className="text-decoration-none" style={{}}>
+              Services
+            </Link>
+          </li>
+          <li className="c">
+            <Link to="/products" className="text-decoration-none" style={{}}>
+              Products
+            </Link>
+          </li>
+          <li className="c">
+            <Link to="/team" className="text-decoration-none" style={{}}>
+              Team
+            </Link>
+          </li>
+          <li className="c">
+            <Link to="/blogs" className="text-decoration-none" style={{}}>
+              Blogs
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="menu-bar " onClick={() => showMenu()}>
+        <MdMenu size={30} />
+      </div>
+      <div className="cta d-lg-flex gap login-btn-wrapper d-none ">
+        <button className="buttonlightgreen }">Login</button>
+        <button className="buttongreen">Sign Up</button>
+      </div>
+    </Container>
+  );
 };
 
 export default Navbar;
