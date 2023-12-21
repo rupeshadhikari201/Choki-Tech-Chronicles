6f4437d6c70e981706a043850e3c223e607b6d8b
YaredAlex <yareda25@gmail.com>
2023-12-21 18:04:22 +0530

    adding multiple card to service

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,11 +1,40 @@
 import Card from "../Components/Card.jsx";
+import "../Css/service.css";
 const Sevices = () => {
-	return (
-		<div>
-			<h1>Our Services</h1>
-			<Card />
-		</div>
-	);
+  return (
+    <div className="service-section">
+      <h1 className="text-center p-3">Our Services</h1>
+      <div
+        className={`card-holder
+	 d-flex gap-3
+	 justify-content-center
+	 flex-wrap
+	  `}
+      >
+        <Card
+          title={"Web Development"}
+          description={`Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry&apos;s standard dummy text
+          ever since the 1500s`}
+          imgPath={"./cardone.png"}
+        />
+        <Card
+          title={"Web Development"}
+          description={`Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry&apos;s standard dummy text
+          ever since the 1500s`}
+          imgPath={"./cardtwo.svg"}
+        />
+        <Card
+          title={"Web Development"}
+          description={`Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry&apos;s standard dummy text
+          ever since the 1500s`}
+          imgPath={"./cardthree.svg"}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Sevices;
