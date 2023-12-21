6de3de00936164ac62317d32eb130100bf314b48
YaredAlex <yareda25@gmail.com>
2023-12-22 01:00:14 +0530

    modify scroll and word

--- src/Pages/OurTeam.jsx
+++ src/Pages/OurTeam.jsx
@@ -28,7 +28,7 @@ const OurTeam = () => {
           }
         />
         <TeamCard
-          name={"Diviya"}
+          name={"Divya"}
           position={"Social Media Manager"}
           description={` Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. `}
