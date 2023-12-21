fd25770abf9ff14b3204a6319a896df9c560b3d6
YaredAlex <yareda25@gmail.com>
2023-12-21 23:15:58 +0530

    our team component created

---
+++ src/Pages/OurTeam.jsx
@@ -0,0 +1,22 @@
+const OutTeam = () => {
+  return (
+    <div>
+      <h1>Meet Our Team</h1>
+      <p>gGet to Know talented individuals behind our company</p>
+      <div className="team-card-holder">
+        <div className="card-3">
+          <div>
+            <img src={""} alt="team-member" />
+          </div>
+          <div>
+            <h1>Choki Dorgi</h1>
+            <h5>Founder and CEO</h5>
+          </div>
+          <div className="cont"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OutTeam;
