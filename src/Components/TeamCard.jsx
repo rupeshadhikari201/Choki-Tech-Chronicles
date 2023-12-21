d869551cd8169d72229eb68b444ab7937f002d30
YaredAlex <yareda25@gmail.com>
2023-12-22 00:35:14 +0530

    making card reusable

--- src/Components/TeamCard.jsx
+++ src/Components/TeamCard.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa6";
 
-const TeamCard = () => {
-  return <div>TeamCard</div>;
+const TeamCard = ({ name, position, description, img }) => {
+  return (
+    <div className="card-3">
+      <div className="card-3-img mb-4">
+        <img src={img} alt="team-member" />
+      </div>
+      <div className="card-3-body">
+        <h1>{name}</h1>
+        <h6 className="mb-3">{position}</h6>
+        <p>{description}</p>
+      </div>
+      <div className="card-3-icon-wrapper d-flex justify-content-around">
+        <FaGithub className="icon" size={30} />
+        <FaLinkedin className="icon" size={30} />
+        <FaInstagram className="icon" size={30} />
+      </div>
+    </div>
+  );
 };
 
 export default TeamCard;
