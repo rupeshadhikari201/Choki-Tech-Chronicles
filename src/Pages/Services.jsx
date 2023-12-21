59efb7bf72df8508587f2cba7e6909cd698b4ad7
YaredAlex <yareda25@gmail.com>
2023-12-21 18:56:47 +0530

    adding research img

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,9 +1,21 @@
 import Cards from "../Components/Card.jsx";
 import "../Css/service.css";
+import research from "../assets/scientific-research.png";
 const Sevices = () => {
   return (
-    <div className="service-section">
-      <h1 className="text-center p-3">Our Services</h1>
+    <div className="service-section pb-5">
+      <div className="d-flex justify-content-center align-items-center">
+        <h1
+          className="text-center p-3 
+		 
+		"
+        >
+          Our Services
+        </h1>
+        <div className="">
+          <img alt="research" src={research} />
+        </div>
+      </div>
       <div
         className={`card-holder
 	 d-flex gap-3
