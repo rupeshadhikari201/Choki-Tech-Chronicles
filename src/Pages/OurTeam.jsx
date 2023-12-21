4167505ff316dc4d6ece0f12555e9cd36d26309e
YaredAlex <yareda25@gmail.com>
2023-12-21 23:37:15 +0530

    adding image and padding

--- src/Pages/OurTeam.jsx
+++ src/Pages/OurTeam.jsx
@@ -2,19 +2,29 @@ import "../Css/ourteam.css";
 
 const OurTeam = () => {
   return (
-    <section className="our-team">
+    <section className="our-team px-3 pb-3">
       <h1 className="text-center">Meet Our Team</h1>
       <p className="text-center">
         Get to Know talented individuals behind our company
       </p>
-      <div className="team-card-holder">
+      <div className="team-card-holder d-flex justify-content-center">
         <div className="card-3">
-          <div>
-            <img src={""} alt="team-member" />
+          <div className="card-3-img mb-4">
+            <img
+              src={
+                "https://images.pexels.com/photos/19554104/pexels-photo-19554104/free-photo-of-a-man-taking-a-photo-of-the-sunset-on-the-beach.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              }
+              alt="team-member"
+            />
           </div>
-          <div>
+          <div className="card-3-body">
             <h1>Choki Dorgi</h1>
-            <h5>Founder and CEO</h5>
+            <h6 className="mb-3">Founder and CEO</h6>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s
+            </p>
           </div>
           <div className="cont"></div>
         </div>
