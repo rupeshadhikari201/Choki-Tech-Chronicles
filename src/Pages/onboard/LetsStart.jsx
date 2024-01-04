db66638a62795282df235c391076b23e9ab13d71
YaredAlex <yareda25@gmail.com>
2024-01-04 06:17:08 +0530

    onboarding

--- src/Pages/onboard/LetsStart.jsx
+++ src/Pages/onboard/LetsStart.jsx
@@ -1,5 +1,60 @@
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../utils/context/auth";
+import "../../Css/onboarding/onboarding.css";
 const LetsStart = () => {
-  return <div>LetsStart</div>;
+  const { state } = useContext(AuthContext);
+  useEffect(() => {
+    console.log(state);
+  }, []);
+  return (
+    <div
+      className="bg-light-green
+  min-height-100vh 
+  d-flex align-items-center 
+  justify-content-center
+  "
+    >
+      <div
+        className="
+        wrapper
+        bg-white-variant-2
+        p-4
+        rounded
+        d-flex flex-column
+        align-items-center
+        "
+      >
+        <h1>Hello, {state.user?.firstName}</h1>
+        <h4 className="text-md">let&apos;s know more about you</h4>
+        <button
+          className="
+         btn-custom
+         transparent
+         text-black-variant-1
+         border-primary-green
+         mt-3
+         mb-0
+         text-md
+         "
+        >
+          I am here to hire
+        </button>
+        <button
+          className="
+         btn-custom
+         transparent
+         text-black-variant-1
+         border-primary-green
+         mt-3
+         mb-4
+         text-md
+         "
+        >
+          Work as agent
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default LetsStart;
