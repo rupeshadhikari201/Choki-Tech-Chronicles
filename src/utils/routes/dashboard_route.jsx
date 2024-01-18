287225c706a4f7bed7f745fffc6bb86c63dccb78
YaredAlex <yareda25@gmail.com>
2024-01-19 00:34:00 +0530

    making layout for dashboard routes

--- src/utils/routes/dashboard_route.jsx
+++ src/utils/routes/dashboard_route.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
-
+import DashBoardLeft from "../../Pages/dashboard/dashboard_left_side";
+import DashBoardRight from "../../Pages/dashboard/dashboard_right_side";
+import "../../Css/dashboard/dashboard.css";
 const DashBoardRoute = () => {
-  return <Outlet />;
+  const [showNav, setShowNav] = useState(false);
+  return (
+    <div
+      className="
+  min-height-100vh
+  bg-light-green
+  d-flex
+  "
+    >
+      {/* LeftSide */}
+      <DashBoardLeft setShowNav={setShowNav} showNav={showNav} />
+      {/* RightSide */}
+      <DashBoardRight showNav={showNav} setShowNav={setShowNav}>
+        <Outlet />
+      </DashBoardRight>
+      {/*  */}
+    </div>
+  );
 };
 
 export default DashBoardRoute;
