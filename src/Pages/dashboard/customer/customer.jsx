b298b4ee39ff17c1494672462f249d9a72b0702e
YaredAlex <yareda25@gmail.com>
2024-01-19 00:28:37 +0530

    adding dashboard cards

--- src/Pages/dashboard/customer/customer.jsx
+++ src/Pages/dashboard/customer/customer.jsx
@@ -1,9 +1,16 @@
+import { useContext } from "react";
+import { AuthContext } from "../../../utils/context/auth";
+import { Link } from "react-router-dom";
+import DashboardCard from "../../../Components/card/dashboardCards";
+import { Edit2, Money, TickCircle } from "iconsax-react";
+
 const CustomerDashBoard = () => {
+  const { userState } = useContext(AuthContext);
+
   return (
     <div className="">
       <div
         className="d-flex
-      flex-md-row
       flex-column
      gap-4
      px-3
@@ -13,9 +20,51 @@ const CustomerDashBoard = () => {
         }}
       >
         {/* Greeting */}
-        <div>Hello there</div>
+        <h2 className="text-blue-variant-1 mb-0 text-capitalize font-weight-400">
+          Hello {userState.user?.firstName}!
+        </h2>
+        <Link
+          className={` bg-light-lime-secondary
+          text-black-variant-2
+          btn-custom-variant-3
+        my-0
+        ms-0
+        w-auto
+        text-xsm
+        rounded
+        `}
+          style={{
+            padding: "4px 10px",
+          }}
+        >
+          New project
+        </Link>
+
+        {/*  */}
+        <div
+          className={`d-flex justify-content-center justify-content-sm-between flex-wrap gap-2 `}
+          style={{ maxWidth: "900px" }}
+        >
+          <DashboardCard
+            title={"Posted Project"}
+            number={4}
+            icon={<Edit2 size={25} color="blue" />}
+          />
+          <DashboardCard
+            title={"Compeleted Project"}
+            number={2}
+            icon={<TickCircle size={25} />}
+          />
+          <DashboardCard
+            title={"Investment"}
+            number={300}
+            icon={<Money size={25} color="green" />}
+          />
+        </div>
         {/* Activity */}
         <Activity />
+        {/* Invitation */}
+        <Invitation />
       </div>
     </div>
   );
@@ -32,13 +81,10 @@ function Activity() {
     col
     p-2
     "
-      style={{
-        minHeight: "300px",
-      }}
     >
-      <p>Activity</p>
+      <h5 className={`font-weight-400`}>Activity</h5>
       <div>
-        <p className="font-weight-bold">Recent Activity</p>
+        <p className="font-weight-bold m-0">Recent Activity</p>
         <div
           className="ps-2
           mb-2
@@ -49,9 +95,25 @@ function Activity() {
           <p className="m-0">No recent activity</p>
         </div>
       </div>
+    </div>
+  );
+}
+function Invitation() {
+  return (
+    <div
+      className="
+text-black-variant-2
+dashboard-activity
+bg-light-lime-secondary
+col
+p-2
+"
+      style={{ maxWidth: "400px" }}
+    >
       <div>
-        <h5>Invitations</h5>
+        <h5 className={`font-weight-400`}>Invitations</h5>
       </div>
+      <p className={`text-center`}>No Invitation So far</p>
     </div>
   );
 }
