c23d2409cb0de5720675aceb6d8f7712bb423551
YaredAlex <yareda25@gmail.com>
2024-01-19 00:33:14 +0530

    changing state to userState

--- src/utils/context/auth.jsx
+++ src/utils/context/auth.jsx
@@ -4,14 +4,14 @@ import { ACTION_TYPE } from "../../reducer/action/action";
 
 export const AuthContext = createContext();
 const AuthContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(authReducer, {});
+  const [userState, userdispatch] = useReducer(authReducer, {});
   useEffect(() => {
     //cheking if there is a user in the local storage
-    dispatch({ type: ACTION_TYPE.LOCAL_STORAGE_EXTRACTOIN });
+    userdispatch({ type: ACTION_TYPE.LOCAL_STORAGE_EXTRACTOIN });
     console.log(" auth context effect called");
   }, []);
   return (
-    <AuthContext.Provider value={{ state, dispatch }}>
+    <AuthContext.Provider value={{ userState, userdispatch }}>
       {children}
     </AuthContext.Provider>
   );
