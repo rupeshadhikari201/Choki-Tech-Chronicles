863e85894e0d0db3353489171f916c4388afa425
YaredAlex <yareda25@gmail.com>
2024-02-13 22:07:22 +0530

    adding sidebar and main-container top-bar

--- src/Pages/dashboard/dashboard.jsx
+++ src/Pages/dashboard/dashboard.jsx
@@ -1,15 +1,47 @@
 import "../../Css/dashboard/dashboard.css";
-
-const DashBoard = () => {
-  // const [showNav, setShowNav] = useState(true);
+import SideBar from "./dashboard_sidebar";
+import DashBoardTopbar from "./dashboard_topbar";
+const DashBoard = ({ children }) => {
   return (
     <div
       className="
   min-height-100vh
-  bg-light-green
+  bg-dark-blue
   d-flex
   "
-    ></div>
+    >
+      {/* Leftside */}
+      <SideBar />
+      {/* Right side */}
+      <div
+        className={`
+      position-fixed
+      bg-blue
+      border border-3
+      p-2
+      bg-white
+      rounded
+      `}
+        style={{
+          top: "20px",
+          bottom: "20px",
+          left: "200px",
+          right: "20px",
+          overflowY: "scroll",
+        }}
+      >
+        <div
+          className={`position-relative w-100 mb-2 mb-md-0`}
+          style={{ height: "60px", backgroundColor: "#FAFAFA" }}
+        >
+          {/* TopBar */}
+          <DashBoardTopbar />
+
+          {/* end of top fixed */}
+        </div>
+        {children}
+      </div>
+    </div>
   );
 };
 
