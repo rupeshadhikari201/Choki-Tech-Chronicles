bde75641875ec23ca795cc43840cf476ee66914c
YaredAlex <yareda25@gmail.com>
2024-02-13 22:08:53 +0530

    layout for routing pages

--- src/utils/routes/dashboard_route.jsx
+++ src/utils/routes/dashboard_route.jsx
@@ -1,25 +1,28 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
-import DashBoardLeft from "../../Pages/dashboard/dashboard_left_side";
-import DashBoardRight from "../../Pages/dashboard/dashboard_right_side";
+import DashBoardLeft from "../../Pages/dashboard/dashboard_sidebar";
+import DashBoardRight from "../../Pages/dashboard/dashboard_topbar";
 import "../../Css/dashboard/dashboard.css";
+import DashBoard from "../../Pages/dashboard/dashboard";
 const DashBoardRoute = () => {
   const [showNav, setShowNav] = useState(false);
   return (
     <div
       className="
-  min-height-100vh
-  bg-light-green
+      bg-dark-blue
   d-flex
   "
     >
-      {/* LeftSide */}
+      {/* LeftSide 
       <DashBoardLeft setShowNav={setShowNav} showNav={showNav} />
-      {/* RightSide */}
+      {/* RightSide 
       <DashBoardRight showNav={showNav} setShowNav={setShowNav}>
         <Outlet />
       </DashBoardRight>
-      {/*  */}
+      */}
+      <DashBoard>
+        <Outlet />
+      </DashBoard>
     </div>
   );
 };
