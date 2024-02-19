5f17151d92c6cd0e82402dd2bc3c2b331402598c
YaredAlex <yareda25@gmail.com>
2024-02-19 11:29:11 +0530

    cleaning structure

--- src/utils/routes/dashboard_route.jsx
+++ src/utils/routes/dashboard_route.jsx
@@ -1,25 +1,13 @@
-import { useState } from "react";
 import { Outlet } from "react-router-dom";
-import DashBoardLeft from "../../Pages/dashboard/dashboard_sidebar";
-import DashBoardRight from "../../Pages/dashboard/dashboard_topbar";
 import "../../Css/dashboard/dashboard.css";
 import DashBoard from "../../Pages/dashboard/dashboard";
 const DashBoardRoute = () => {
-  const [showNav, setShowNav] = useState(false);
   return (
     <div
       className="
       bg-dark-blue
-  d-flex
   "
     >
-      {/* LeftSide 
-      <DashBoardLeft setShowNav={setShowNav} showNav={showNav} />
-      {/* RightSide 
-      <DashBoardRight showNav={showNav} setShowNav={setShowNav}>
-        <Outlet />
-      </DashBoardRight>
-      */}
       <DashBoard>
         <Outlet />
       </DashBoard>
