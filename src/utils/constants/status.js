f9c8b9f8abc2e134e9cc5eef6b0bb5664b3c28ec
YaredAlex <yareda25@gmail.com>
2024-02-28 23:35:28 +0530

    adding project constant

--- src/utils/constants/status.js
+++ src/utils/constants/status.js
@@ -3,3 +3,14 @@ export const STATUS = {
   CHECKED: "CHECKED",
   PENDING: "PENDING",
 };
+export const projectDummyData = [
+  // Add more dummy data entries here...
+  {
+    title: "Project 10",
+    created: "2024-02-10",
+    payment: "Pending",
+    progress: "In Progress",
+    submission: "2024-02-25",
+    budget: "$3000",
+  },
+];
