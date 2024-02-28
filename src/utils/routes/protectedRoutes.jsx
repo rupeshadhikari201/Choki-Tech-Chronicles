4e2ad884c74f04c214e89ee7203d8147037c5ea4
YaredAlex <yareda25@gmail.com>
2024-02-28 23:43:43 +0530

    checking tooken

--- src/utils/routes/protectedRoutes.jsx
+++ src/utils/routes/protectedRoutes.jsx
@@ -1,15 +1,62 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { commonPath } from "../constants/path";
-import { getUserFromLocal } from "../../reducer/local/local";
-import { useEffect } from "react";
+import { base_url, commonPath } from "../constants/path";
+import ReactLoading from "react-loading";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { ACTION_TYPE } from "../../reducer/action/action";
+import { AuthContext } from "../context/auth";
 
 const ProtectedRoutes = () => {
-  const { user } = getUserFromLocal({});
+  const [currentUser, setCurrentUser] = useState("hello there");
+  const { userdispatch } = useContext(AuthContext);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
+    setLoading(true);
+    const profile = async () => {
+      axios
+        .get(base_url + "/api/user/profile/", {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${Cookies.get("token")}`,
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+          setCurrentUser({
+            firstName: res.data.data.firstname,
+            lastName: res.data.data.lastname,
+            email: res.data.data.email,
+          });
+          return res.data;
+        })
+        .catch((e) => {
+          if (e.response.status == 401) {
+            console.log("response is 401");
+            userdispatch({
+              type: ACTION_TYPE.ERASE_LOCALE,
+            });
+            navigator(`${commonPath}/signin`);
+          }
+          console.log(e.response.data);
+        })
+        .finally((res) => {
+          setLoading(false);
+        });
+    };
+    profile();
     console.log("protected");
   }, []);
 
-  return user?.auth ? <Outlet /> : <Navigate to={`/${commonPath}/signin`} />;
+  return loading ? (
+    <div className="text-black-variant-2 position-absolute w-100 h-100 d-flex justify-content-center align-items-center bg-dark-blue">
+      <ReactLoading type="spin" height={50} width={50} />
+    </div>
+  ) : currentUser ? (
+    <Outlet />
+  ) : (
+    <Navigate to={`/${commonPath}/signin`} />
+  );
 };
 
 export default ProtectedRoutes;
