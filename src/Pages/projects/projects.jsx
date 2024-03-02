11f2943f7315078b503621952b9038ca98df6c6e
YaredAlex <yareda25@gmail.com>
2024-03-02 17:41:06 +0530

    removed projectDummy data

--- src/Pages/projects/projects.jsx
+++ src/Pages/projects/projects.jsx
@@ -1,61 +1,20 @@
+import { useContext, useEffect } from "react";
 import CustomerProjectTable from "../dashboard/customer/projects";
+import { ProjectContext } from "../../utils/context/project";
 
 const Projects = () => {
-  const dummyData = [
-    {
-      title: "Project 1",
-      created: "2024-02-01",
-      payment: "Paid",
-      progress: "Completed",
-      submission: "2024-02-10",
-      budget: "$1000",
-    },
-    {
-      title: "Project 6",
-      created: "2024-02-01",
-      payment: "Paid",
-      progress: "Completed",
-      submission: "2024-02-10",
-      budget: "$1000",
-    },
-    {
-      title: "Project 7",
-      created: "2024-02-01",
-      payment: "Paid",
-      progress: "Completed",
-      submission: "2024-02-10",
-      budget: "$1000",
-    },
-    {
-      title: "Project 2",
-      created: "2024-02-02",
-      payment: "Pending",
-      progress: "In Progress",
-      submission: "2024-02-12",
-      budget: "$1500",
-    },
-    {
-      title: "Project 3",
-      created: "2024-02-03",
-      payment: "Paid",
-      progress: "Completed",
-      submission: "2024-02-15",
-      budget: "$2000",
-    },
-    // Add more dummy data entries here...
-    {
-      title: "Project 10",
-      created: "2024-02-10",
-      payment: "Pending",
-      progress: "In Progress",
-      submission: "2024-02-25",
-      budget: "$3000",
-    },
-  ];
+  const { projectData, projectDispatch } = useContext(ProjectContext);
+  useEffect(() => {
+    console.log("Data from projects page");
+    console.log(projectData);
+  }, []);
   return (
     <div className={`text-black-variant-1 ps-2`}>
       <h5 className={`font-weight-400`}>All Projects</h5>
-      <CustomerProjectTable data={dummyData} />
+      <CustomerProjectTable
+        data={projectData.data}
+        projectDispatch={projectDispatch}
+      />
     </div>
   );
 };
