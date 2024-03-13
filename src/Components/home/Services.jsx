2c9fca2ea73210c7f928f04f0528bc0ade13f0e3
YaredAlex <yareda25@gmail.com>
2024-03-13 22:04:04 +0530

    changed the service design

--- src/Components/home/Services.jsx
+++ src/Components/home/Services.jsx
@@ -1,90 +1,235 @@
-import Cards from "../card/Card.jsx";
 import "../../Css/home/service.css";
-import { motion, useScroll, useSpring, useTransform } from "framer-motion";
-import { useRef } from "react";
+import { motion, useScroll, useSpring, useInView } from "framer-motion";
+import { useInsertionEffect, useRef } from "react";
+import {
+  Graph,
+  Message,
+  Monitor,
+  People,
+  SmsTracking,
+  TickCircle,
+  UserTag,
+} from "iconsax-react";
 const Sevices = () => {
   const item = [
     {
-      title: "service1",
+      title: "Development",
+      subtitle: `Crafting custom web solutions tailored to your needs, from websites to complex web applications, using the latest technologies for optimal performance and user experience.`,
+      icon: <SmsTracking />,
     },
     {
-      title: "service2",
+      title: "Monitoring",
+      subtitle: `Providing real-time insights into your website's performance, uptime, and security to ensure it remains accessible, responsive, and secure, with proactive monitoring and timely issue resolution.`,
+      icon: <Monitor />,
+    },
+    {
+      title: " Consulting on Tech Services",
+      subtitle: `Offering expert guidance and advice on technology services, including web development, cloud computing, cybersecurity, and digital marketing, to help you make informed decisions and stay ahead of the competition.`,
+      icon: <Graph />,
+    },
+    {
+      title: "Easy Collaboration",
+      subtitle: `Facilitating seamless collaboration between clients and freelancers, streamlining communication, project management, and feedback processes for efficient and productive workflows.`,
+      icon: <People />,
+    },
+    {
+      title: "Detailed Analytics",
+      subtitle: `Delivering comprehensive analytics and reporting on website traffic, user behavior, and performance metrics to gain actionable insights and optimize your online presence for maximum impact and results.`,
+      icon: <Graph />,
     },
   ];
-  const ref = useRef();
-  const { scrollYProgress } = useScroll({ target: ref });
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 200,
-    damping: 50,
+  const serviceRef = useRef();
+  const colabFeatureRef = useRef();
+  const isInView = useInView(serviceRef, { once: true });
+  const colabFeatureInView = useInView(colabFeatureRef, {
+    once: true,
+    margin: "-100px",
   });
+  const variant = {
+    show: {
+      y: 0,
+      transition: {
+        duration: 1,
+        staggerChildren: 0.2,
+      },
+    },
+    hidden: {
+      y: 50,
+    },
+  };
+  const variantFeature = {
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 1,
+        staggerChildren: 0.2,
+      },
+    },
+    hidden: {
+      y: 20,
+      opacity: 0,
+    },
+  };
   return (
     <div
       className="service-section pb-4 pb-md-5 pt-3"
       style={{ position: "relative" }}
-      ref={ref}
     >
-      {/* <div className="d-flex justify-content-center align-items-center">
+      <div className="px-4">
         <h1 className="text-center p-3 text-black-variant-1">Our Services</h1>
-         <div className="d-none d-md-block">
-          <img alt="research" src={research} />
-        </div> 
-      </div> */}
-      <div className="px-4" style={{ position: "sticky", top: 0, left: "0" }}>
-        <h1 className="text-center p-3 text-black-variant-1">Our Services</h1>
-        <motion.div
-          style={{ scaleX: scaleX, height: "10px", backgroundColor: "white" }}
-          className=""
-        ></motion.div>
       </div>
-      <SingleService />
-      <SingleService />
+
+      <motion.section
+        ref={serviceRef}
+        variants={variant}
+        initial={"hidden"}
+        animate={isInView ? "show" : ""}
+        className="d-flex mx-auto px-4 px-sm-2 mt-4 gap-4 service-card-wrapper flex-column flex-md-row"
+      >
+        {item &&
+          item.map((section, index) => (
+            <motion.div
+              variants={variant}
+              key={index}
+              className="rounded service-card  p-2 px-4 d-flex flex-column text-black-variant-1"
+            >
+              <div className="mt-3 mb-3 icon-wrapper">{section.icon}</div>
+              <h5 className="font-weight-500">{section.title}</h5>
+              <div className="text-black-variant-3">{section.subtitle}</div>
+            </motion.div>
+          ))}
+      </motion.section>
+      {/*  */}
+      {/* Features */}
+      <section
+        className=" text-black-variant-1 mx-4 feature-section mx-auto "
+        style={{ marginTop: "100px" }}
+      >
+        <h5>Features</h5>
+
+        {/* Feature1 ^ */}
+        <div>
+          <motion.div
+            ref={colabFeatureRef}
+            variants={variantFeature}
+            initial="hidden"
+            animate={colabFeatureInView ? "show" : ""}
+            className="d-flex align-items-center gap-4 flex-column flex-md-row px-4 px-md-2"
+          >
+            <motion.div variants={variantFeature} className="col-12 col-md-6">
+              <motion.div
+                variants={variantFeature}
+                style={{ maxWidth: "500px" }}
+              >
+                <h1 className="font-weight-500" style={{}}>
+                  Collaboration for large Team
+                </h1>
+                <p className="text-black-variant-3">
+                  Our collaboration platform provides secure access control,
+                  real-time communication, and efficient task management,
+                  enhancing productivity and teamwork for large teams.
+                </p>
+              </motion.div>
+              <motion.div variants={variantFeature}>
+                <motion.div
+                  variants={variantFeature}
+                  className="mb-3 d-flex gap-2 p-4 align-items-center rounded feature-list-wrapper"
+                >
+                  <People />{" "}
+                  <p className="mb-0">
+                    Perfect for individual and larget Teams
+                  </p>
+                </motion.div>
+                <motion.div
+                  variants={variantFeature}
+                  className="mb-3 d-flex gap-2 p-4 align-items-center rounded feature-list-wrapper"
+                >
+                  <Message /> <p className="mb-0">Easy to Use Communication</p>
+                </motion.div>
+                <motion.div
+                  variants={variantFeature}
+                  className="mb-3 d-flex gap-2 p-4  align-items-center rounded feature-list-wrapper"
+                >
+                  <UserTag /> <p className="mb-0">Rapid Support</p>
+                </motion.div>
+              </motion.div>
+            </motion.div>
+            <motion.div
+              variants={variantFeature}
+              className="col"
+              style={{ height: "400px", overflow: "hidden" }}
+            >
+              <img
+                src="assets/dash-board.png"
+                className="w-100"
+                style={{ objectFit: "cover" }}
+              />
+            </motion.div>
+          </motion.div>
+          {/* Feature 2 */}
+          <div className="" style={{ marginTop: "100px" }}>
+            <div className="d-flex align-items-center gap-4 flex-column flex-md-row px-4 px-md-2">
+              <div
+                className="col order-2 order-md-1"
+                style={{ height: "300px", overflow: "hidden" }}
+              >
+                <img
+                  src="assets/dash-board.png"
+                  className="w-100"
+                  style={{ objectFit: "cover" }}
+                />
+              </div>
+              <motion.div variants={variantFeature} className="col">
+                <motion.div
+                  variants={variantFeature}
+                  initial={"hidden"}
+                  whileInView={"show"}
+                  viewport={{ once: true, amount: 0.5 }}
+                >
+                  <h1 className="font-weight-500" style={{}}>
+                    Detail analytics
+                  </h1>
+                  <p className="text-black-variant-3">
+                    Our analytics dashboard delivers customizable visual
+                    insights, real-time data tracking, and actionable metrics,
+                    empowering informed decision-making and continuous
+                    improvement.
+                  </p>
+                </motion.div>
+                <motion.div
+                  variants={variantFeature}
+                  initial={"hidden"}
+                  whileInView={"show"}
+                  viewport={{ once: true, amount: 0.5 }}
+                >
+                  <div className="mb-1 d-flex gap-2 p-4 py-3 align-items-center border-0 rounded feature-list-wrapper">
+                    <TickCircle />{" "}
+                    <p className="mb-0">
+                      Customizable visual insights for informed decision-making.
+                    </p>
+                  </div>
+                  <div className="mb-1 d-flex gap-2 p-4 py-3 align-items-center border-0 rounded feature-list-wrapper">
+                    <TickCircle />{" "}
+                    <p className="mb-0">
+                      Real-time data tracking for timely responses.
+                    </p>
+                  </div>
+                  <div className="mb-3 d-flex gap-2 p-4 py-3 align-items-center border-0 rounded feature-list-wrapper">
+                    <TickCircle />{" "}
+                    <p className="mb-0">
+                      Actionable metrics drive continuous improvement.
+                    </p>
+                  </div>
+                </motion.div>
+              </motion.div>
+            </div>
+          </div>
+        </div>
+        {/* End of features */}
+      </section>
     </div>
   );
 };
 
 export default Sevices;
-
-const SingleService = () => {
-  const ref = useRef();
-  const { scrollYProgress } = useScroll({ target: ref });
-  const y = useTransform(scrollYProgress, [0, 1], [-200, 200]);
-  return (
-    <section
-      className={`text-black-variant-1 d-flex justify-content-center align-items-center gap-4`}
-      style={{
-        scrollSnapAlign: "start",
-        height: "100vh",
-      }}
-    >
-      <motion.div
-        style={{
-          maxWidth: "500px",
-          height: "50%",
-        }}
-        ref={ref}
-      >
-        <img
-          style={{
-            height: "100%",
-            width: "100%",
-            objectFit: "cover",
-            objectPosition: "top",
-          }}
-          src={`https://thumbs.dreamstime.com/b/digital-transformation-technology-strategy-ideas-adoption-business-age-enhancing-global-capabilities-215266548.jpg`}
-          alt="img"
-        />
-      </motion.div>
-      <motion.div
-        style={{
-          maxWidth: "500px",
-          y: y,
-        }}
-        className={`d-flex flex-column gap-3`}
-      >
-        <h4 style={{ fontSize: "3rem" }}>Development</h4>
-        Developing website for search and find what you are looking for solution
-        for cutting edge tech
-      </motion.div>
-    </section>
-  );
-};
