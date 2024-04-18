78b3fe56f6580a917887730cd75ec5b040200c03
YaredAlex <yareda25@gmail.com>
2024-04-18 11:47:39 +0530

    checking user type to show project

--- src/Pages/projects/projects.jsx
+++ src/Pages/projects/projects.jsx
@@ -1,22 +1,33 @@
 import { useContext, useEffect } from "react";
 import CustomerProjectTable from "../dashboard/customer/projects";
 import { ProjectContext } from "../../utils/context/project";
+import PostedProjects from "../dashboard/agent/projects";
+import { AuthContext } from "../../utils/context/auth";
 
 const Projects = () => {
   const { projectData, projectDispatch } = useContext(ProjectContext);
-  useEffect(() => {
-    console.log("Data from projects page");
-    console.log(projectData);
-  }, []);
-  return (
-    <div className={`text-black-variant-1 ps-2`}>
-      <h5 className={`font-weight-400`}>All Projects</h5>
-      <CustomerProjectTable
-        data={projectData.data}
-        projectDispatch={projectDispatch}
-      />
-    </div>
-  );
+  const { userState } = useContext(AuthContext);
+  useEffect(() => {}, []);
+  if (userState.user.user_type == "client")
+    return (
+      <div className={`text-black-variant-1 ps-2`}>
+        <h5 className={`font-weight-400`}>All Projects</h5>
+        <CustomerProjectTable
+          data={projectData.data}
+          projectDispatch={projectDispatch}
+        />
+      </div>
+    );
+  else
+    return (
+      <div className={`text-black-variant-1 ps-2`}>
+        <h5 className={`font-weight-400`}>All Projects</h5>
+        <PostedProjects
+          data={projectData.data}
+          projectDispatch={projectDispatch}
+        />
+      </div>
+    );
 };
 
 export default Projects;
