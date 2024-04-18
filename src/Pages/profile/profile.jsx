65ece64f3c5eae5809db84ac73b0bba27036b749
YaredAlex <yareda25@gmail.com>
2024-04-18 11:46:41 +0530

    checking user type

--- src/Pages/profile/profile.jsx
+++ src/Pages/profile/profile.jsx
@@ -1,14 +1,21 @@
 import { useContext } from "react";
 import { AuthContext } from "../../utils/context/auth";
 import CustomerProfile from "../dashboard/customer/profile";
+import AgentProfile from "../dashboard/agent/profile";
 const Profile = () => {
   const { userState } = useContext(AuthContext);
-
-  return (
-    <div>
-      <CustomerProfile />
-    </div>
-  );
+  if (userState.user.user_type == "client")
+    return (
+      <div>
+        <CustomerProfile />
+      </div>
+    );
+  else
+    return (
+      <div>
+        <AgentProfile />
+      </div>
+    );
 };
 
 export default Profile;
