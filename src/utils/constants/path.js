452f49bd4cf76be5e5c43f893d3ead820169cac7
Rupesh <21bcs11201@gmail.com>
2025-04-13 23:46:10 +0530

    added logo, modifed base_url with /

--- src/utils/constants/path.js
+++ src/utils/constants/path.js
@@ -1,4 +1,4 @@
 // export const commonPath = "Choki-Tech-Chronicles";
 export const commonPath = "";
-export const base_url = "https://gokap.onrender.com";
+export const base_url = "https://gokap.onrender.com/";
 // export const base_url = "http://localhost:8000/";
